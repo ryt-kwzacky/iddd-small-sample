93c5d3873173cb270842b110ea554e5bac8ae238
RyutoKawasaki <various.use.0416@gmail.com>
2020-08-06 11:20:22 +0900

    organize package

--- src/main/kotlin/com/createuser/core/domain/models/user/User.kt
+++ src/main/kotlin/com/createuser/core/domain/models/user/User.kt
@@ -2,5 +2,5 @@ package com.createuser.core.domain.models.user
 
 import com.createuser.dddFoundation.domain.Entity
 
-class User(private val id: UserId, private val name: UserName): Entity() {
+class User(private val id: UserId, private val name: UserName) : Entity() {
 }
--- src/main/kotlin/com/createuser/core/domain/models/user/UserName.kt
+++ src/main/kotlin/com/createuser/core/domain/models/user/UserName.kt
@@ -2,4 +2,17 @@ package com.createuser.core.domain.models.user
 
 import com.createuser.dddFoundation.domain.ValueObject
 
-class UserName(value: String): ValueObject()
+data class UserName(private val value: String) : ValueObject() {
+    companion object {
+        const val MINIMUM_USER_NAME_LENGTH = 3
+        const val MAXIMUM_USER_NAME_LENGTH = 16
+    }
+
+    init {
+        validateFields()
+    }
+
+    private fun validateFields() {
+        require(value.length in MINIMUM_USER_NAME_LENGTH..MAXIMUM_USER_NAME_LENGTH)
+    }
+}
