1bb176a780c4ce401b19e720ba4bdab5f8774a88
RyutoKawasaki <various.use.0416@gmail.com>
2020-08-06 11:32:19 +0900

    add info to UserId

--- src/main/kotlin/com/createuser/core/domain/models/user/UserId.kt
+++ src/main/kotlin/com/createuser/core/domain/models/user/UserId.kt
@@ -2,4 +2,16 @@ package com.createuser.core.domain.models.user
 
 import com.createuser.dddFoundation.domain.ValueObject
 
-class UserId(value: String): ValueObject()
+class UserId(private val value: String): ValueObject() {
+    companion object {
+        const val IDENTIFY_LENGTH = 28
+    }
+
+    init {
+        validateFields()
+    }
+
+    private fun validateFields() {
+        require(value.length == IDENTIFY_LENGTH)
+    }
+}
